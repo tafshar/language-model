b312b4607926d2fd33c5531ae6f56415c2b19941
tafshar <tara.afshar@gmail.com>
2020-12-08 21:15:31 -0800

    generate translations

--- custom_layers/mt_model.py
+++ custom_layers/mt_model.py
@@ -4,56 +4,39 @@ from tensorflow.python.keras import initializers
 from tensorflow.keras.layers import Layer
 
 
-class MtModel(Layer):
+class MtModel(tf.keras.Model):
     
     def __init__(self, vocab_size: int, emb_dim: int, rnn_units: int):
+        super().__init__()
         self.embedding_layer = tf.keras.layers.Embedding(vocab_size, emb_dim)
         
-        self.encoder_lstm = tf.keras.layers.LSTM(rnn_units, return_sequences = False, return_state=True)
-        self.decoder_lstm = tf.keras.layers.LSTM(rnn_units, return_state=True)
+        self.encoder_lstm = tf.keras.layers.LSTM(rnn_units, return_sequences=False, return_state=True)
+        self.decoder_lstm = tf.keras.layers.LSTM(rnn_units, return_sequences=True, return_state=True, stateful = False)
         self.decoder_output_layer = tf.keras.layers.Dense(vocab_size)
 
-    def forward(self, src_batch, tgt_batch):
+    def call(self, src_batch, tgt_batch):
+        """ tgt_batch: [batch_size, seq_len] """
+        
         src_embedding = self.embedding_layer(src_batch)
         tgt_embedding = self.embedding_layer(tgt_batch)
 
         src_encoder, final_memory_state, final_carry_state = self.encoder_lstm(src_embedding)
-        tgt_decoder = self.decoder_lstm(tgt_embedding, initial_state = final_memory_state)
+        tgt_decoder, tgt_final_memory_state, tgt_final_carry_state = self.decoder_lstm(tgt_embedding,
+                                        initial_state=(final_memory_state, final_carry_state))
 
         decoder_output = self.decoder_output_layer(tgt_decoder)
         return decoder_output
-        
+
+    def get_final_encoder_states(self, src_batch: tf.Tensor) -> [tf.Tensor, tf.Tensor]: 
+        final_src_emb = self.embedding_layer(src_batch)
+        final_encoder, final_memory_state, final_carry_state = self.encoder_lstm(final_src_emb)
+        return final_memory_state, final_carry_state
 
 
+    def next_step_decoder(self, tgt_example, final_memory_state, final_carry_state) -> tf.Tensor:
+        tgt_emb = self.embedding_layer(tgt_example)
+        final_tgt_decoder, final_tgt_memory_state, final_tgt_carry_state = self.decoder_lstm(tgt_emb, initial_state=(final_memory_state, final_carry_state))
+        final_decoder_output = self.decoder_output_layer(final_tgt_decoder)
+        return final_decoder_output, final_tgt_memory_state, final_tgt_carry_state
 
-        
-
-
-
-
-
-
-
-
-
-
-
-#def build_encoder(vocab_size, embedding_dim, rnn_units, batch_size):
-#  model = tf.keras.Sequential([
-#    EmbeddingLayer(vocab_size, embedding_dim),
-#    tf.keras.layers.LSTM(rnn_units, return_state=True),
-#  ])
-#  return model
-#######LSTM EX
-##lstm = tf.keras.layers.LSTM(256, return_state=True)
-##output, final_memory_state = lstm(src_dataset[0])
-##print(final_memory_state.shape)
-#def build_decoder(vocab_size, embedding_dim, rnn_units, batch_size):
-#  model = tf.keras.Sequential([
-#    EmbeddingLayer(vocab_size, embedding_dim),
-#    custom_layers.lstm.LSTM(rnn_units),
-#    #tf.keras.layers.LSTM(rnn_units), 
-#    # WITH CALL ARGUMENT initial state = final_memory_state
-#  ])
-#  return model
 
--- translation_model.py
+++ translation_model.py
@@ -5,7 +5,6 @@ from typing import List
 import random
 import numpy as np
 import os
-#import custom_layers.simple_rnn
 import custom_layers.lstm
 import time
 from custom_layers.emedding_layer import EmbeddingLayer
@@ -15,6 +14,9 @@ from custom_layers.mt_model import MtModel
 src = open("mt/TaraData/applied.short.train.src.txt", "r").read()
 trg = open("mt/TaraData/applied.short.train.src.txt", "r").read()
 
+##################
+#### source and target processing
+##################
 
 combined = src + trg
 combined_subwords = combined.split()
@@ -33,6 +35,7 @@ sub2idx = {u:i for i, u in enumerate(vocab, start=2)}
 # End of sentence
 EOS_ID = 0
 sub2idx["<EOS>"] = EOS_ID
+
 # Start of sentence
 SOS_ID = 1
 sub2idx["<SOS>"] = SOS_ID
@@ -55,12 +58,14 @@ for s in sentences_trg:
   s = s.strip().split()
   trg_tokens.append([sub2idx[c] for c in s])
 
+################# 
+##### Create batches
+#################
+
 def sort_lists_by_length(src_data: List[List[int]], tgt_data: List[List[int]]):
     data_tuples = [(a, b) for a, b in zip(src_data, tgt_data)]
-    # data tuple: ( [1,2,3], [5, 6, 8, 0])
     return sorted(data_tuples, key=lambda x: len(x[1]))
 
-#sorted_list_of_tokens = list(sorted(list_of_tokens, key = len))
 sorted_list_of_tokens = sort_lists_by_length(src_tokens, trg_tokens)
 
 #divide list into lists of size n
@@ -80,7 +85,6 @@ def create_batches(sorted_list_of_examples: List[List[int]], batch_size: int):
   return list_of_batches
 
 #all data split into chunks of 20
-#list_of_batches = create_batches(sorted_list_of_tokens, 20)
 src_data = [src_tuple[0] for src_tuple in sorted_list_of_tokens]
 trg_data = [trg_tuple[1] for trg_tuple in sorted_list_of_tokens]
 
@@ -89,28 +93,28 @@ trg_batches = create_batches(trg_data, 20)
 
 seq_length = 20
 
-#how many sequences for corpus
-#Not used
 examples_per_epoch = len(combined)//(seq_length+1)
 
 
 #function to duplicate and shift source/target
 def split_input_target(chunk):
-    # offset whole batch!
     input_text = chunk[:, :-1]
     target_text = chunk[:, 1:]
     return input_text, target_text
 
-#map the sequences to the function to create input and target
+
+##################
+#### Create source and target input datasets
+##################
+
 src_dataset = [split_input_target(x) for x in src_batches]
 trg_dataset = [split_input_target(x) for x in trg_batches]
 
-#first example in dataset shifted
+
 input_example_batch, target_example_batch = src_dataset[-1]
 input_example = input_example_batch[0]
 target_example = target_example_batch[0]
-print('Input data: ', ' '.join([idx2sub[x] for x in input_example]))
-print('Target data:', ' '.join([idx2sub[x] for x in target_example]))
+
   
 BATCH_SIZE = 64
 BUFFER_SIZE = 10000
@@ -122,60 +126,46 @@ embedding_dim = 256
 #RNN units
 rnn_units = 256
 
+##################
+#### Train step
+################## 
 
 model = MtModel(vocab_size, embedding_dim, rnn_units)
 
 src_input_example_batch, src_target_example_batch = src_dataset[-1]
 example_batch_predictions = model(input_example_batch, target_example_batch)
-print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
 
-##already built model, feed it our batches, iterate over batches 
-
-
-print(model.summary())
 
 sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
 sampled_indices = tf.squeeze(sampled_indices,axis=-1).numpy()
 
 
 checkpoint_dir = './training_checkpoints'
-# Name of the checkpoint files
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
 
 checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_prefix,
     save_weights_only=True)
 
-#history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
 optimizer = tf.keras.optimizers.Adam()
 
 def train_step(inp, target):
-  #looking at gradient (derivative) of loss function and optimize the weights of nn
-  #calculating the loss within training loop so we can see how each prediciton and loss changes per epoch/batch
   with tf.GradientTape() as tape:
     predictions = model(inp, target)
-    #forward calc
     loss = tf.reduce_mean(
         tf.keras.losses.sparse_categorical_crossentropy(
             target, predictions, from_logits=True))
-  #what calculate gradients with respect to the model
   grads = tape.gradient(loss, model.trainable_variables)
   #update parameters
   optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
   return loss
 
-# Training step
 EPOCHS = 10
 
 for epoch in range(EPOCHS):
   start = time.time()
 
-  # initializing the hidden state at the start of every epoch
-  # initally hidden is None
-  #hidden = model.reset_states()
-
-  # shuffle after each epoch
   random.shuffle(src_dataset) 
   for (batch_n, (inp, target)) in enumerate(src_dataset):
     loss = train_step(inp, target)
@@ -184,37 +174,31 @@ for epoch in range(EPOCHS):
       template = 'Epoch {} Batch {} Loss {}'
       print(template.format(epoch+1, batch_n, loss))
 
-  # saving (checkpoint) the model every 5 epochs
   if (epoch + 1) % 5 == 0:
     model.save_weights(checkpoint_prefix.format(epoch=epoch))
 
 
-# model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
 print ('Epoch {} Loss {:.4f}'.format(epoch+1, loss))
 print ('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
 
 model.save_weights(checkpoint_prefix.format(epoch=epoch))
 
-
+##################
+#### Generate translation
+##################
 
 def generate_text(model, source_sentence):
 
   #characters to generate
     num_generate = 1000
 
-  #convert to integer representation
     source_sentence = source_sentence.split(' ')
     input_eval = [SOS_ID] + [sub2idx[s] for s in source_sentence]
     input_eval = tf.expand_dims(input_eval, 0)
 
     final_memory_state, final_carry_state = model.get_final_encoder_states(input_eval)
-    #model.set_initial_decoder_states(final_memory_state, final_carry_state)
-
-    text_generated = []
 
     temperature = 1.0
-
-    #model.reset_states()
 
     decoder_input = [[SOS_ID]]
     translation_id = []
@@ -228,7 +212,6 @@ def generate_text(model, source_sentence):
         predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
         decoder_input = [[predicted_id]]
        
-        #breakpoint()
         translation_id.append(idx2sub[predicted_id])
         
         if predicted_id == 0:
